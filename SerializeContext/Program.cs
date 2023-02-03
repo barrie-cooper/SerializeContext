36f921c6527d7b0c22f7f26f8df81fbe2a783ff2
Barrie Cooper <barrie.cooper@aveva.com>
2023-02-03 16:10:18 +0000

    Add project files.

---
+++ SerializeContext/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SignalR;
+using Moq;
+
+try
+{
+    var context = new Mock<HttpContext>().Object;
+    JsonSerializer.Serialize(context);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
+
+Console.ReadLine();
+
+try
+{
+    var context = new Mock<HubCallerContext>().Object;
+    JsonSerializer.Serialize(context);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
