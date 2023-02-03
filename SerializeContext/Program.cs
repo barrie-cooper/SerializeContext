718fa16fb6cd32d1e29bda762fd9a094444b1a04
Barrie Cooper <barrie.cooper@aveva.com>
2023-02-03 16:23:59 +0000

    Tidy

--- SerializeContext/Program.cs
+++ SerializeContext/Program.cs
@@ -1,11 +1,14 @@
-﻿using System.Text.Json;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SignalR;
 using Moq;
+using System.Text.Json;
 
 try
 {
-    var context = new Mock<HttpContext>().Object;
+    Console.WriteLine();
+    Console.WriteLine("Enter to test HubCallerContext serialize");
+    Console.ReadLine();
+    var context = new Mock<HubCallerContext>().Object;
     JsonSerializer.Serialize(context);
 }
 catch (Exception ex)
@@ -13,11 +16,12 @@ catch (Exception ex)
     Console.WriteLine(ex);
 }
 
-Console.ReadLine();
-
 try
 {
-    var context = new Mock<HubCallerContext>().Object;
+    Console.WriteLine();
+    Console.WriteLine("Enter to test HttpContext serialize");
+    Console.ReadLine();
+    var context = new Mock<HttpContext>().Object;
     JsonSerializer.Serialize(context);
 }
 catch (Exception ex)
